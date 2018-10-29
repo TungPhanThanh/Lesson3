4e75d68b9743356a03b9e45dcf190b530777026c
Tung Phan <tungphanhunre@gmail.com>
2018-10-29 13:22:40 +0700

    Exercise 3

---
+++ app/src/main/java/android/tungpt/intenfilepermission/ModelImage.java
@@ -0,0 +1,15 @@
+package android.tungpt.intenfilepermission;
+
+import android.net.Uri;
+
+public class ModelImage {
+    private Uri mUri;
+
+    public Uri getUri() {
+        return mUri;
+    }
+
+    public void setUri(Uri uri) {
+        mUri = uri;
+    }
+}
